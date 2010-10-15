c860370928a9b2920f0116608ca5f939a3dc5586
Don Cruse <doncruse@gmail.com>
2010-10-14 20:40:12 -0500

    Simplifying method for determining following status

--- app.rb
+++ app.rb
@@ -150,13 +150,7 @@ get '/show' do
   joined_ids = mutual_follower_ids(my_follows, other_follows)
   @joined = populate_mutual_followers(joined_ids)
 
-  friendship = @client.friendships.show.json? :target_screen_name => @otheruser
-
-  return "Friendship object:\n\n#{friendship.to_hash.inspect}"
-
   @following = do_they_follow_you(@otheruser, @id)
-
-  return "Did the join\n\n#{@following.inspect}"
 
   erb :results
   end
